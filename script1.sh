5602da564a194501c3370833b7edc10198ec1d1f
Tom Hlava <email = hlava@cesnet.cz>
2021-01-05 19:35:31 +0100

    v8

--- script1.sh
+++ script1.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+WSP=${1:-/home/runner/work/test1/test1}
+
 echo "actions-test/script1.sh"
+pwd
+ls -la
+
+echo ""
+echo "$WSP:"
+ls -la "$WSP"
+
+echo ""
+echo "scr2:"
+$WSP/scripts/scr2.sh par1
+echo "actions-test/script1.sh done"
